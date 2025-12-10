187b519dd4ca68a4bccb750da66fca5e6b9cce41
James Yab <james.luu.yab@gmail.com>
2025-12-09 22:48:31 -0500

    Update the Conan and CMakeLists.txt build system to work with CLion

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,23 @@
+# This file is managed by Conan, contents will be overwritten.
+# To keep your changes, remove these comment lines, but the plugin won't be able to modify your requirements
+
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import cmake_layout, CMakeToolchain
 
-
-class ServerRecipe(ConanFile):
+class ConanApplication(ConanFile):
+    package_type = "application"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
-    options = {"test": [True, False]}
-    default_options = {"test": False}
-
-    def requirements(self):
-        if self.options.test:
-            self.requires("gtest/1.17.0")
-
-    def build(self):
-        cmake = CMake(self)
-        defs = {
-            "ENABLE_TESTS": "ON" if self.options.test else "OFF"
-        }
-        cmake.configure(variables=defs)
-        cmake.build()
+    generators = "CMakeDeps"
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
+
+    def requirements(self):
+        requirements = self.conan_data.get('requirements', [])
+        for requirement in requirements:
+            self.requires(requirement)
