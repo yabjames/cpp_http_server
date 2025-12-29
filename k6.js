e871d9c9b5335a45a7488c62c20f7314fad70066
James Yab <james.luu.yab@gmail.com>
2025-12-29 17:14:47 -0500

    Implement the new k6.js benchmark script

--- k6.js
+++ k6.js
@@ -1,30 +1,25 @@
 import http from "k6/http";
-import { check, sleep } from "k6";
+import { check } from "k6";
 
 export const options = {
   thresholds: {
     http_req_failed: [{ threshold: "rate<0.01", abortOnFail: true }], // availability threshold for error rate
     http_req_duration: ["p(99)<1000"], // Latency threshold for percentile
   },
-  // define scenarios
   scenarios: {
-    breaking: {
-      executor: "ramping-vus",
-      stages: [
-        { duration: "10s", target: 20 },
-        { duration: "10s", target: 40 },
-        { duration: "10s", target: 80 },
-        { duration: "10s", target: 320 },
-        { duration: "10s", target: 1000 },
-        { duration: "10s", target: 5000 },
-        //....
-      ],
+    constant_request_rate: {
+      executor: "constant-arrival-rate",
+      rate: 30000,
+      timeUnit: "1s", // 1000 iterations per second, i.e. 1000 RPS
+      duration: "10s",
+      preAllocatedVUs: 100, // how large the initial pool of VUs would be
+      maxVUs: 20000, // if the preAllocatedVUs are not enough, we can initialize more
     },
   },
 };
 
 export default function () {
-  const res = http.get("http://localhost:3490/");
+  const res = http.post("http://localhost:3490/test2/123/foo/432");
   check(res, {
     "response code was 200": (res) => res.status == 200,
   });
